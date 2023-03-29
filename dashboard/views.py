7b49de7c32d345a1bc0863c477a7706177a9dcd6
Umair <68957587+warraich-umair@users.noreply.github.com>
2023-03-29 23:23:33 +0500

    graph error fixing

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,24 @@
-# dashboard/views.py
+from django.shortcuts import render
 
+# Create your views here.
 from django.shortcuts import render
 from dashboard.models import Nids
 
 
 def dashboard(request):
-    nids = Nids.objects.order_by('-timestamp')[:25]
-    return render(request, 'dashboard.html', {'nids': nids})
+    # Retrieve data from the database
+    data = Nids.objects.values('timestamp', 'total_fwd_packet_size')
+
+    # Transform the data into the format expected by Chart.js
+    labels = []
+    values = []
+    for row in data:
+        labels.append(str(row['timestamp']))
+        values.append(row['total_fwd_packet_size'])
+
+    # Pass the data to the template
+    context = {
+        'labels': labels,
+        'values': values,
+    }
+    return render(request, 'dashboard.html', context)
