090f93c1a8e711db946366d4a4c284db10e9676d
Umair <68957587+warraich-umair@users.noreply.github.com>
2023-03-29 23:59:16 +0500

    graph error fixed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('fetch_data/', views.fetch_data, name='fetch_data'),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 # views.py
 
 from django.shortcuts import render
+from django.http import JsonResponse
 from dashboard.models import Nids
 
 
@@ -21,3 +22,24 @@ def dashboard(request):
         'values': values,
     }
     return render(request, 'dashboard.html', context)
+
+
+def fetch_data(request):
+    # Retrieve data from the database
+    data = Nids.objects.values('timestamp', 'total_fwd_packet_size')
+
+    # Transform the data into the format expected by Chart.js
+    labels = []
+    values = []
+    for row in data:
+        labels.append(str(row['timestamp']))
+        values.append(row['total_fwd_packet_size'])
+
+    # Create a dictionary containing the labels and values
+    chart_data = {
+        'labels': labels,
+        'values': values
+    }
+
+    # Return the data as a JSON response
+    return JsonResponse(chart_data)
